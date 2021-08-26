0ffccefbe8a6bbb53ddfbd4991647323f1a656c9
Brandon Bloch <brandon@bloch.ca>
2021-08-26 16:08:24 -0400

    Basic STL to 3MF conversion example

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"github.com/hpinc/go3mf"
+	"github.com/hpinc/go3mf/importer/stl"
 	"log"
+	"os"
 )
 
+func loadSTL(path string) (model *go3mf.Model, err error) {
+	model = new(go3mf.Model)
+	file, openErr := os.Open(path)
+	if openErr != nil {
+		err = openErr
+		return
+	}
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			err = closeErr
+		}
+	}()
+	reader := bufio.NewReader(file)
+	decoder := stl.NewDecoder(reader)
+	if decodeErr := decoder.Decode(model); decodeErr != nil {
+		err = decodeErr
+		return
+	}
+	return
+}
+
 func main() {
-	model := new(go3mf.Model)
+	model, err := loadSTL("/Users/brandonbloch/Downloads/cube.stl")
+	if err != nil {
+		log.Fatalln(err)
+	}
 	writer, err := go3mf.CreateWriter("/Users/brandonbloch/Desktop/3mf.zip")
 	if err != nil {
 		log.Fatalln(err)
