edb12716c9f073c57d36a3d52006e558dc2c8410
Karsan <karsan.uthayakumar@gmail.com>
2025-05-16 11:34:05 -0400

    basic test setup

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func getOpts() (Opts, error) {
 	return opts, nil
 }
 
-func run() {
+func Run() {
 	opts, err := getOpts()
 	if err != nil {
 		log.Fatalln(err)
@@ -104,5 +104,5 @@ func run() {
 }
 
 func main() {
-	run()
+	Run()
 }
